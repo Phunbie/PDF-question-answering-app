ce94f6d07d09a6b3e75bdf95a420dce2a65d82e2
Phunbie <adeneyeoluwafunbi@gmail.com>
2023-04-26 09:04:30 +0000

    Add comment

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template, request 
 from PyPDF2 import PdfReader #PdfFileReader
+from langchain import PromptTemplate
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS #ElasticVectorSearch, Pinecone, Weaviate, FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 import os 
+
+prompt = PromptTemplate(
+        input_variables=["question", "docs"],
+        template="""
+        You are a helpful assistant that that can answer questions about document based on the input documents.
+        
+        Answer the following question: {question}
+        By searching the following documents: {docs}
+        
+        Only use the factual information from the transcript to answer the question.
+        
+        If you feel like you don't have enough information to answer the question, say "I don't know".
+        """,
+)
 
 
 raw_text=""
@@ -16,6 +31,7 @@ api=""
 baseDirectory=os.getcwd()
 
 def convert2text(pdfFile):
+  """Converts PDF file to texts and return the text"""
   rText=""
   name= pdfFile.filename
   filePath= os.path.join('uploads',name)
@@ -30,6 +46,8 @@ def convert2text(pdfFile):
 
 
 def textSplitter(text):
+  """Splits large text to chuncks"""
+
   text_splitter = CharacterTextSplitter(        
         separator = "\n",
         chunk_size = 1000,
@@ -40,6 +58,8 @@ def textSplitter(text):
   return text
 
 def qAndA(text,api,query):
+  """Given an OpenAI API key, a document and a question
+  this function will return an answer"""
   embeddings = OpenAIEmbeddings(openai_api_key=api)
   docsearch = FAISS.from_texts(text, embeddings)
   chain = load_qa_chain(OpenAI(openai_api_key=api), chain_type="stuff")
